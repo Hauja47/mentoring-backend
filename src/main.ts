0a2599bb82c82b8d0851e0aa34a7b9edbce3c4cc
Hauja47 <hoangletrunghau01@gmail.com>
2023-10-23 00:00:07 +0700

    Setup nestjs

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Config and setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Mentoring API')
+    .setDescription('This is API of Mentoring')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
