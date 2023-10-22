029224abe9a5174f8bb31844a950fcb93afcbd96
Hauja47 <hoangletrunghau01@gmail.com>
2023-10-23 00:25:48 +0700

    Auto generate swagger.json

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as path from 'path';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +14,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // Generate Swagger json file
+  const swaggerOutputPath = path.resolve(process.cwd(), 'swagger.json');
+  writeFileSync(swaggerOutputPath, JSON.stringify(document), {
+    encoding: 'utf-8',
+  });
 
   await app.listen(3000);
 }
